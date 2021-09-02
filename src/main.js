4bcaa3a5d56f4c470b56fc94702b1e127b2aeb35
Its07 <82678937+EWE07@users.noreply.github.com>
2021-09-02 14:36:59 +0000

    Events and commands in different files

--- src/main.js
+++ src/main.js
@@ -1,13 +1,35 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const { readdirSync } = require('fs');
 
-client.once('ready', () => {
-	console.log('Bot Running');
-});
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
 
-client.on('message', message => {
-	if (message.author.bot) return;
-	if (message.content === '!ping') return message.channel.send('Pong!');
+const events = readdirSync('./src/events').filter(file => file.endsWith('.js'));
+for (let file of events) {
+	try {
+		let pull = require(`./events/${file}`);
+		pull.event = pull.event || file.replace('.js', '');
+		client.on(pull.event, pull.bind(null, client));
+	} catch (err) {
+		console.log(err);
+	}
+}
+
+readdirSync('./src/commands').forEach(dir => {
+	const commands = readdirSync(`./src/commands/${dir}/`).filter(file =>
+		file.endsWith('.js')
+	);
+	for (let file of commands) {
+		let pull = require(`./commands/${dir}/${file}`);
+		if (pull.name) {
+			client.commands.set(pull.name, pull);
+		} else {
+			continue;
+		}
+		if (pull.aliases && Array.isArray(pull.aliases))
+			pull.aliases.forEach(alias => client.aliases.set(alias, pull.name));
+	}
 });
 
 client.login(process.env.token);
