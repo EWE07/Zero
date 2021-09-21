1453168325b928c68f6467aa0cfe1f391a62d2e0
07 <ewertonbarretolima07@gmail.com>
2021-09-21 20:29:15 -0300

    Update node.js

--- src/ZeroNode/node.js
+++ src/ZeroNode/node.js
@@ -2,5 +2,5 @@ const process = require('process');
 
 process.on('warning', (warning) => { 
    console.warn(`ZeroNode: [${warning.name}] 
-   (${warning.stack}) ${warning.message}`)
+   (${warning.stack}) ${warning.message}`);
 });
