b930dfabbb5570d44501c88e20f2dc5154f83708
07 <ewertonbarretolima07@gmail.com>
2021-09-22 09:03:24 -0300

    Update message.js

--- src/ZeroBot/events/message.js
+++ src/ZeroBot/events/message.js
@@ -3,6 +3,7 @@ const { prefix } = require("../../../config.json");
 
 module.exports = (client, message) => {
   if (message.author.bot || message.author.webhook) return;
+  if (message.channel.type === 'dm') return 
 
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
   const cmd = args.shift().toLowerCase();
